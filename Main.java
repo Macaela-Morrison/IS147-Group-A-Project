d8ad0fb2c4676adab6d58eae833c375165037ad2
Macaela-Morrison <mmorris6@umbc.edu>
2024-10-21 18:28:40 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,26 @@
+import java.util.*;
+
 public class Main {
+
+    static void displayMenu()
+    {
+
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Test");
         
         System.out.print("test Lillian");
+        Scanner userInput = new Scanner(System.in);
+        String input = "";
+        System.out.println("Choose an option");
+        input = userInput.nextLine();
+        displayMenu();
+
+
     }
 
 }
