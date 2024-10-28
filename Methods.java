db310feb25d982b0c98d62799e64d6e16ba918d3
AngryDaniel79 <Dowolab1@umbc.edu>
2024-10-27 23:25:07 -0400

    Create Methods.java

---
+++ Methods.java
@@ -0,0 +1,9 @@
+// Methods
+
+// calcTotalExpenses() - Calculate total expenses across all categories
+
+// calcRecurringExpenses() - Calculate the total of the recurring expenses
+
+// calcNonRecurringExpenses() - Calculate the total of the non-recurring expenses
+
+// calcRemainingBalance() - Will keep a running total of the amount of money the user has left. Will be displayed on the main menu. 
